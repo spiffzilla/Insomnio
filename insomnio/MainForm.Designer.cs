7b0d61ce10c02e1b020fdb7afa07294d8fca717a
Jörgen Bertholdsson <githubb@bertholdsson.com>
2015-02-24 07:10:16 +0100

    First commit

---
+++ insomnio/MainForm.Designer.cs
@@ -0,0 +1,155 @@
+﻿namespace insomnio
+
+{
+    partial class frmInsomnio
+
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmInsomnio));
+            this.chkEnableDisable = new System.Windows.Forms.CheckBox();
+            this.timMove = new System.Windows.Forms.Timer(this.components);
+            this.trkTime = new System.Windows.Forms.TrackBar();
+            this.lblSeconds = new System.Windows.Forms.Label();
+            this.chkMore = new System.Windows.Forms.CheckBox();
+            this.trkOpacity = new System.Windows.Forms.TrackBar();
+            this.lblOpacity = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.trkTime)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trkOpacity)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // chkEnableDisable
+            // 
+            this.chkEnableDisable.AutoSize = true;
+            this.chkEnableDisable.Location = new System.Drawing.Point(12, 8);
+            this.chkEnableDisable.Name = "chkEnableDisable";
+            this.chkEnableDisable.Size = new System.Drawing.Size(59, 17);
+            this.chkEnableDisable.TabIndex = 0;
+            this.chkEnableDisable.Text = "Enable";
+            this.chkEnableDisable.UseVisualStyleBackColor = true;
+            this.chkEnableDisable.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // timMove
+            // 
+            this.timMove.Interval = 1000;
+            this.timMove.Tick += new System.EventHandler(this.timMove_Tick);
+            // 
+            // trkTime
+            // 
+            this.trkTime.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.trkTime.Location = new System.Drawing.Point(77, 16);
+            this.trkTime.Maximum = 500;
+            this.trkTime.Minimum = 1;
+            this.trkTime.Name = "trkTime";
+            this.trkTime.Size = new System.Drawing.Size(195, 45);
+            this.trkTime.TabIndex = 3;
+            this.trkTime.TickStyle = System.Windows.Forms.TickStyle.None;
+            this.trkTime.Value = 1;
+            this.trkTime.Scroll += new System.EventHandler(this.trkTime_Scroll);
+            this.trkTime.ValueChanged += new System.EventHandler(this.trkTime_ValueChanged);
+            // 
+            // lblSeconds
+            // 
+            this.lblSeconds.AutoSize = true;
+            this.lblSeconds.Location = new System.Drawing.Point(278, 16);
+            this.lblSeconds.Name = "lblSeconds";
+            this.lblSeconds.Size = new System.Drawing.Size(63, 13);
+            this.lblSeconds.TabIndex = 4;
+            this.lblSeconds.Text = "labSeconds";
+            // 
+            // chkMore
+            // 
+            this.chkMore.AutoSize = true;
+            this.chkMore.Location = new System.Drawing.Point(12, 23);
+            this.chkMore.Name = "chkMore";
+            this.chkMore.Size = new System.Drawing.Size(50, 17);
+            this.chkMore.TabIndex = 5;
+            this.chkMore.Text = "More";
+            this.chkMore.UseVisualStyleBackColor = true;
+            this.chkMore.CheckedChanged += new System.EventHandler(this.chkMore_CheckedChanged);
+            // 
+            // trkOpacity
+            // 
+            this.trkOpacity.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.trkOpacity.Location = new System.Drawing.Point(77, 67);
+            this.trkOpacity.Maximum = 100;
+            this.trkOpacity.Minimum = 10;
+            this.trkOpacity.Name = "trkOpacity";
+            this.trkOpacity.Size = new System.Drawing.Size(195, 45);
+            this.trkOpacity.TabIndex = 6;
+            this.trkOpacity.TickStyle = System.Windows.Forms.TickStyle.None;
+            this.trkOpacity.Value = 40;
+            this.trkOpacity.ValueChanged += new System.EventHandler(this.trkOpacity_ValueChanged);
+            // 
+            // lblOpacity
+            // 
+            this.lblOpacity.AutoSize = true;
+            this.lblOpacity.Location = new System.Drawing.Point(278, 67);
+            this.lblOpacity.Name = "lblOpacity";
+            this.lblOpacity.Size = new System.Drawing.Size(53, 13);
+            this.lblOpacity.TabIndex = 7;
+            this.lblOpacity.Text = "lblOpacity";
+            // 
+            // frmInsomnio
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(354, 108);
+            this.Controls.Add(this.lblOpacity);
+            this.Controls.Add(this.trkOpacity);
+            this.Controls.Add(this.chkMore);
+            this.Controls.Add(this.lblSeconds);
+            this.Controls.Add(this.trkTime);
+            this.Controls.Add(this.chkEnableDisable);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(500, 150);
+            this.MinimumSize = new System.Drawing.Size(374, 79);
+            this.Name = "frmInsomnio";
+            this.Text = "Insomnio";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.trkTime)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trkOpacity)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.CheckBox chkEnableDisable;
+        private System.Windows.Forms.Timer timMove;
+        private System.Windows.Forms.TrackBar trkTime;
+        private System.Windows.Forms.Label lblSeconds;
+        private System.Windows.Forms.CheckBox chkMore;
+        private System.Windows.Forms.TrackBar trkOpacity;
+        private System.Windows.Forms.Label lblOpacity;
+    }
+}
+
---
+++ insomnio/MainForm.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace insomnio
+{
+    public partial class frmInsomnio : Form
+    {
+        int moveX = 1;
+        int delay = 240;
+        int minTime = 1;
+        int maxTime = 300;
+        int tickFreq = 100;
+        double opacity = 100;
+
+        public frmInsomnio()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            chkEnableDisable.Checked = false;
+            EnableDisable();
+            InitializeTrackBar();
+            InitGUI();
+        }
+
+        public void InitGUI()
+        {
+            this.Text = "Insomnio 1.0 © 2015 Jörgen Bertholdsson";
+            this.Size = new Size(374, 93); 
+            lblOpacity.Text = "Opacity";
+            trkOpacity.Value = (int)opacity;
+            trkTime.Value = delay;
+            if (delay > 1)
+            {
+                lblSeconds.Text = delay.ToString() + " sekunder"; 
+            }
+            else 
+            { 
+                lblSeconds.Text = delay.ToString() + " sekund"; 
+            }
+        }
+        
+        public void EnableDisable()
+        {
+            if (chkEnableDisable.Checked)
+            { 
+                timMove.Interval = delay * 1000;
+                timMove.Enabled = true;
+            }
+            else
+            { 
+                timMove.Enabled = false;
+            }
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            EnableDisable();
+        }
+
+        private void MoveCursor()
+        {
+            System.Windows.Forms.SendKeys.SendWait("+");
+            if (moveX == 1) { moveX = -1; }
+            else { moveX = 1; }
+            this.Cursor = new Cursor(Cursor.Current.Handle);
+            Cursor.Position = new Point(Cursor.Position.X - moveX, Cursor.Position.Y);
+        }
+
+        private void timMove_Tick(object sender, EventArgs e)
+        {
+            MoveCursor();
+        }
+
+        private void trkTime_Scroll(object sender, EventArgs e)
+        {
+            //
+        }
+
+        private void trkTime_RightToLeftChanged(object sender, EventArgs e)
+        {
+            //
+        }
+
+        private void trkTime_ValueChanged(object sender, EventArgs e)
+        {
+            delay = (int)(System.Math.Round(trkTime.Value / 1.0));
+            timMove.Interval = delay * 1000;
+             if (delay > 1) { lblSeconds.Text = delay.ToString() + " sekunder"; }
+            else { lblSeconds.Text = delay.ToString() + " sekund"; }
+        }
+
+        private void trkOpacity_ValueChanged(object sender, EventArgs e)
+        {
+            opacity = (System.Math.Round(trkOpacity.Value / 1.0));
+            this.Opacity = opacity / 100; 
+        }
+
+
+        private void InitializeTrackBar()
+        {
+            trkTime.TickStyle = TickStyle.None;
+            trkTime.Minimum = minTime;
+            trkTime.Maximum = maxTime;
+            trkTime.TickFrequency = tickFreq;
+        }
+
+        private void chkMore_CheckedChanged(object sender, EventArgs e)
+        {
+            if (chkMore.Checked)
+            {
+                this.Size = new Size(374, 150);
+            }
+            else
+            {
+                this.Size = new Size(374, 93);
+            }
+        }
+    }
+}
